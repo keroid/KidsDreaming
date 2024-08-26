23f69dbbc213ff75d065011b579fe798acf967a0
kidlxy <lvxinyi@dobest.com>
2024-08-26 14:29:28 +0800

    删除部分文件夹

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+// })
+
+export default {
   plugins: [vue()],
-})
+  base: '/pagetest/',
+  build: {
+    outDir: 'docs'
+  }
+}
